95f55725ffc29f4a0e49615101bfcadbd73ec547
ArtemIlin <artemlex@gmail.com>
2020-05-04 23:20:41 +0300

    plus method recursion - very slow

--- projects/src/FibonacciNumber/FibonacciMethods.java
+++ projects/src/FibonacciNumber/FibonacciMethods.java
@@ -2,6 +2,11 @@ package FibonacciNumber;
 
 public class FibonacciMethods {
 
-    
+    // Recursion method
+    static long recursionFib(long number) {
+        if (number == 0) return 0;
+        if (number == 1) return 1;
+        return recursionFib(number - 1) + recursionFib(number - 2);
+    }
 
 }
--- projects/src/FibonacciNumber/Main.java
+++ projects/src/FibonacciNumber/Main.java
@@ -3,5 +3,11 @@ package FibonacciNumber;
 public class Main {
     public static void main(String[] args) {
 
+            // test recursion method
+        long start = System.currentTimeMillis();
+        long fibonacci = FibonacciMethods.recursionFib(10);
+        long end   = System.currentTimeMillis();
+        System.out.println("   10 number: " + fibonacci);
+        System.out.println("time program: " + (end - start) + "ms");
     }
 }
